9fb0745e5be52cc09807cdc59110d5189d346cda
Mayur Tembhare <54200398+jarvismayur@users.noreply.github.com>
2022-07-16 08:47:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gridlayout/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.gridlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+    }
+}
